4b042b80799d48d96b54332e446a134237facc4e
Shoyo Kumagai <shyokmg.au@gmail.com>
2024-01-07 11:48:42 +1100

    Updated database.js and added logic for getting all content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,10 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-
   const jateDb = await openDB('jate', 1);
-
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({jate:content});
@@ -30,7 +28,19 @@ export const putDb = async (content) => {
   }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  try {
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch {
+  console.error('getDb not implemented')
+  }
+};
 initdb();
